8f86a4cc2070c0267d8a25ccfaece850fcad4c38
Trey Jamison <trey.jamison@gmail.com>
2026-01-08 21:57:52 -0600

    scaffold property detail content with real data

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,35 @@
-const PropertyPage = ({ params: { id } }) => {
-  return <div>PropertyPage {id}</div>;
+import { FaArrowLeft } from 'react-icons/fa';
+import connectDB from '@/config/database';
+import Link from 'next/link';
+import Property from '@/models/Property';
+import PropertyHeaderImage from '@/components/PropertyHeaderImage';
+
+const PropertyPage = async ({ params: { id } }) => {
+  await connectDB();
+  const property = await Property.findById(id).lean();
+
+  return (
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className='container m-auto py-6 px-6'>
+          <Link
+            href='/properties'
+            className='text-blue-500 hover:text-blue-600 flex items-center'
+          >
+            <FaArrowLeft className='mr-2' /> Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section class='bg-blue-50'>
+        <div class='container m-auto py-10 px-6'>
+          <div class='grid grid-cols-1 md:grid-cols-70/30 w-full gap-6'>
+            Property Info
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default PropertyPage;
